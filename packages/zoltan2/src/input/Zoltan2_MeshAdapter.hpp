205592c1a3f17a16d99b3b6f8686fefd31482590
Michel de Messieres <michel@demessieres.com>
2019-05-10 08:13:57 -0600

    Zoltan2: Fix Pamgen tests

--- packages/zoltan2/src/input/Zoltan2_MeshAdapter.hpp
+++ packages/zoltan2/src/input/Zoltan2_MeshAdapter.hpp
@@ -458,6 +458,25 @@ public:
     getIDsViewOf(getPrimaryEntityType(), Ids);
   }
 
+  void getIDsKokkosView(Kokkos::View<const gno_t *,
+    typename node_t::device_type> &ids) const
+  {
+    // I'm not sure how we'll organize this yet
+    // I want to get the cuda builds running and passing
+    // so first step let's just build the View
+    Kokkos::View<gno_t *, typename node_t::device_type> kokkos_ids("pamgen ids", getLocalNumIDs());
+    typename decltype(kokkos_ids)::HostMirror
+      host_temp_values = Kokkos::create_mirror_view(kokkos_ids);
+    const gno_t * gnos;
+
+    getIDsView(gnos);
+    for(int n = 0; n < getLocalNumIDs(); ++n) {
+      host_temp_values(n) = gnos[n];
+    }
+    Kokkos::deep_copy(kokkos_ids, host_temp_values);
+    ids = kokkos_ids;
+  }
+
   int getNumWeightsPerID() const {
     return getNumWeightsPerOf(getPrimaryEntityType());
   }
@@ -470,6 +489,28 @@ public:
                           int coordDim) const
   {
     getCoordinatesViewOf(getPrimaryEntityType(), coords, stride, coordDim);
+  }
+
+  inline void getCoordinatesKokkosView(
+    Kokkos::View<scalar_t **, Kokkos::LayoutLeft, typename node_t::device_type> & elements) const
+  {
+    // I'm not sure how we'll organize this yet
+    // I want to get the cuda builds running and passing
+    // so first step let's just build the View
+    Kokkos::View<scalar_t **, Kokkos::LayoutLeft, typename node_t::device_type>
+      kokkos_coordinates("pamgen coords", getLocalNumIDs(), getDimension());
+    typename decltype(kokkos_coordinates)::HostMirror
+      host_temp_values = Kokkos::create_mirror_view(kokkos_coordinates);
+    const scalar_t * coords;
+    for(int dim = 0; dim < getDimension(); ++dim) {
+      int stride = -1;
+      getCoordinatesView(coords, stride, dim);
+      for(int n = 0; n < getLocalNumIDs(); ++n) {
+        host_temp_values(n, dim) = coords[n*stride];
+      }
+    }
+    Kokkos::deep_copy(kokkos_coordinates, host_temp_values);
+    elements = kokkos_coordinates;
   }
 
   bool useDegreeAsWeight(int idx) const
